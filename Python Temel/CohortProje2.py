b939d61171573d4320969eca875249584ddd596a
yildiz <kadiryasir.ky@gmail.com>
2022-12-15 13:59:51 +0300

    .

---
+++ Python Temel/CohortProje2.py
@@ -0,0 +1,15 @@
+def reverse_list(lst):
+  if not lst:
+    return []
+
+  if isinstance(lst[0], list):
+    return reverse_list(lst[1:]) + [reverse_list(lst[0])]
+  else:
+    return lst[::-1]
+
+
+lst = [[1, 2], [3, 4], [5, 6, 7]]
+reverse_list(lst)
+
+lst = input()
+print(reverse_list(lst))
