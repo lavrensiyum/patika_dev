4d0fcf1cf71929f17c7fcb24ecf183843cd0ab0f
yildiz <kadiryasir.ky@gmail.com>
2023-01-02 14:48:22 +0300

    .

--- SQL/patika_dev_sql_odev.sql
+++ SQL/patika_dev_sql_odev.sql
@@ -147,7 +147,21 @@ SELECT country_id, COUNT(*) AS city_count FROM city GROUP BY country_id ORDER BY
     FULL JOIN rental r ON c.rental_id = r.rental_id;
 
 -- Odev 11
-    
+    SELECT first_name FROM actor 
+    UNION 
+    SELECT first_name FROM customer;
+
+    SELECT first_name FROM actor
+    INTERSECT
+    SELECT first_name FROM customer;
+
+    SELECT first_name FROM actor
+    EXCEPT
+    SELECT first_name FROM customer;
+
+    SELECT first_name FROM actor UNION ALL SELECT first_name FROM customer;
+    SELECT first_name FROM actor INTERSECT ALL SELECT first_name FROM customer;
+    SELECT first_name FROM actor EXCEPT ALL SELECT first_name FROM customer;
 
 
 
